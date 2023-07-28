fb75f3faaba0a3c3a57a165a8270a4c78d8ca9b4
Eden <lsahlm1eden@gmail.com>
2023-07-28 14:39:50 +0800

    added semanticscholarmethod

--- doi2toml.py
+++ doi2toml.py
@@ -1,4 +1,5 @@
 from pybliometrics.scopus import AbstractRetrieval
+from semanticscholar import SemanticScholar
 from toml import dump
 import subprocess
 import bibtexparser
@@ -21,6 +22,10 @@ with open('dois.txt') as f:
         categories.append(info[2])
         summaries.append(info[3])
 
+def assign(field, value):
+    if toml_dict['article'][str(i + 1)][field] == '' and value != None:
+        toml_dict['article'][str(i + 1)][field] = value
+
 for i in range(0, len(dois)):
     doi = dois[i]
     toml_dict['article'][str(i + 1)] = {
@@ -34,7 +39,9 @@ for i in range(0, len(dois)):
                 'abstract': '',
                 'keywords': '',
             }
+    article_n = f'article.{i + 1}'
     try:
+        print(f'Trying scopus for {article_n}')
         article = AbstractRetrieval(doi, id_type='doi', view='FULL')
         keywords = ''
         authors = []
@@ -46,22 +53,36 @@ for i in range(0, len(dois)):
                 authors.append('{} {}'.format(author[3], author[2]))
         if article.abstract != None:
             abstract = article.abstract[article.abstract.find('.') + 1:]
-        toml_dict['article'][str(i + 1)]['title'] = article.title
-        toml_dict['article'][str(i + 1)]['authors'] = ', '.join(authors)
-        toml_dict['article'][str(i + 1)]['journal'] = article.publicationName
-        toml_dict['article'][str(i + 1)]['abstract'] = abstract
-        toml_dict['article'][str(i + 1)]['keywords'] = keywords
-        print(f'article.{i + 1} succeeded with scopus')
+        assign('title', article.title)
+        assign('authors', ', '.join(authors))
+        assign('journal', article.publicationName)
+        assign('abstract', abstract)
+        assign('keywords', keywords)
+        print(f'{article_n} succeeded with scopus')
     except:
-        try:
-            bib = subprocess.run(['doi2bib', doi], stdout = subprocess.PIPE).stdout
-            article = bibtexparser.parse_string(bib.decode('utf-8')).entries[0].fields_dict
-            toml_dict['article'][str(i + 1)]['title'] = article['title'].value
-            toml_dict['article'][str(i + 1)]['authors'] = ', '.join(article['author'].value.split(' and '))
-            toml_dict['article'][str(i + 1)]['journal'] = article['journal'].value
-            print(f'article.{i + 1} succeeded with doi2bib')
-        except:
-            print(f'article {i + 1} failed with all methods')
+        print(f'{article_n} failed with scopus')
+    try:
+        print(f'Trying doi2bib for {article_n}')
+        bib = subprocess.run(['doi2bib', doi], stdout = subprocess.PIPE).stdout
+        article = bibtexparser.parse_string(bib.decode('utf-8')).entries[0].fields_dict
+        assign('title', article['title'].value)
+        assign('authors', ', '.join(article['author'].value.split(' and ')))
+        assign('journal', article['journal'].value)
+        print(f'article.{i + 1} succeeded with doi2bib')
+    except:
+        print(f'article {i + 1} failed with doi2bib')
+    try:
+        print(f'Trying semanticscholar for {article_n}')
+        sch = SemanticScholar()
+        article = sch.get_paper(doi)
+        authors = ', '.join([a['name'] for a in article.authors])
+        assign('title', article.title)
+        assign('authors', authors)
+        assign('journal', article.journal)
+        assign('abstract', article.abstract)
+        print(f'article.{i + 1} succeeded with semanticscholar')
+    except:
+        print(f'{article_n} failed with semanticscholar')
 
 with open('out.toml', 'w') as f:
     dump(toml_dict, f)
